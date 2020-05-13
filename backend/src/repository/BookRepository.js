3635ab154c3ef5f21a4872f144569b6a54a9e78b
sogoagain <imyong0@gmail.com>
2020-05-13 23:24:25 +0900

    backend: BookRepository

---
+++ backend/src/repository/BookRepository.js
@@ -0,0 +1,19 @@
+import fetch from "node-fetch";
+
+const findByKeywordAndPage = async (keyword, page) => {
+    const query = encodeURI(keyword);
+    const size = 5;
+    const requestURL = `https://dapi.kakao.com/v3/search/book?query=${query}&page=${page}&size=${size}`;
+
+    return await fetch(requestURL, {
+        method: 'GET',
+        headers: {
+            'Authorization': process.env.KEY_KAKAO,
+        },
+    }).then(response => response.json())
+        .catch(err => console.log(err));
+};
+
+module.exports = {
+    findByKeywordAndPage: findByKeywordAndPage
+};
