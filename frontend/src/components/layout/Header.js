6ad3ecb609c8d96f6ccfde724f0feca8d1122731
sogoagain <imyong0@gmail.com>
2020-07-18 13:01:37 +0900

    Implement - Header, NotFoundPage 컴포넌트 추출

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => (
+  <header
+    className="container"
+  >
+    <div
+      className="row bg-light py-2 mb-3 rounded"
+    >
+      <div className="col-md-12">
+        <h3 className="text-center">도서 출처 표기 생성기</h3>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
