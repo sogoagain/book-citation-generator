9e22e93f48bd1a90b0a4e28d8becbeea10c45cff
sogoagain <imyong0@gmail.com>
2020-07-28 23:39:50 +0900

    Refactor - BookItem

--- frontend/src/components/book/BookItem.js
+++ frontend/src/components/book/BookItem.js
@@ -1,31 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import BookDescription from './BookDescription';
 
 const BookItem = ({
-  book,
-  onClick,
-  tabIndex,
-}) => {
-  const onBookClick = () => {
-    onClick(book);
-  };
+  tabIndex, book, onClick,
+}) => (
+  <div
+    onClick={onClick}
+    onKeyDown={onClick}
+    role="button"
+    tabIndex={tabIndex}
+  >
+    <BookDescription book={book} />
+  </div>
+);
 
-  return (
-    <div
-      onClick={onBookClick}
-      onKeyDown={onBookClick}
-      role="button"
-      tabIndex={tabIndex}
-    >
-      <BookDescription book={book} />
-    </div>
-  );
-};
-
-BookItem.propTypes = {
-  book: PropTypes.oneOfType([PropTypes.object]).isRequired,
-  onClick: PropTypes.func.isRequired,
-};
-
-export default React.memo(BookItem);
+export default BookItem;
