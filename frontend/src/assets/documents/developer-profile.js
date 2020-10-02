80d3242a2fb986a67edfdcd8406d429ab3686569
sogoagain <imyong0@gmail.com>
2020-10-02 20:27:41 +0900

    Env - Start the front end renewal

--- frontend/src/assets/documents/developer-profile.js
+++
@@ -1,6 +0,0 @@
-const developerProfile = {
-  title: 'sogoagain',
-  link: 'https://github.com/sogoagain/',
-};
-
-export default developerProfile;
--- frontend/src/pages/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NotFoundPage() {
-  return (
-    <h2>
-      404 Not Found
-    </h2>
-  );
-}
