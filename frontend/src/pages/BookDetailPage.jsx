cd50b45565c295a892e96a10b679d8a73b9b5f61
sogoagain <hello@sogoagain.com>
2025-08-17 21:40:23 +0900

    도서 상세 라우팅을 수정한다

--- frontend/src/pages/BookDetailPage.jsx
+++ frontend/src/pages/BookDetailPage.jsx
@@ -1,21 +1,32 @@
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams, Navigate, useLocation } from 'react-router-dom'
 import { useAppStore } from '../store/useAppStore'
 import { isIsbnMatch } from '../utils/IsbnUtils'
 import BookDetailContainer from '../containers/BookDetailContainer.jsx'
+import { useEffect } from 'react'
 
 const BookDetailPage = () => {
   const { bookId } = useParams()
-  const { bookDetail } = useAppStore()
+  const { bookDetail, showBookDetail } = useAppStore()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.state?.book && !bookDetail.visible) {
+      showBookDetail(location.state.book)
+    }
+  }, [location.state, bookDetail.visible, showBookDetail])
 
   if (
-    !bookDetail.visible ||
-    !bookDetail.book ||
-    !isIsbnMatch(bookDetail.book.isbn, bookId)
+    bookDetail.visible &&
+    bookDetail.book &&
+    isIsbnMatch(bookDetail.book.isbn, bookId)
   ) {
-    return <Navigate to='/' replace />
+    return <BookDetailContainer book={bookDetail.book} />
   }
 
-  return <BookDetailContainer book={bookDetail.book} />
+  if (location.state?.book && isIsbnMatch(location.state.book.isbn, bookId)) {
+    return <BookDetailContainer book={location.state.book} />
+  }
+  return <Navigate to='/' replace />
 }
 
 export default BookDetailPage
